85af597822902a3c07423566dff8951c2630bc3e
markcstansberry <mark.c.stansberry@gmail.com>
2013-06-03 23:15:02 -0700

    first comment

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trialone::Application.config.secret_token = '1b388cab46e2f625daa5b6a956a3e3411f81cb87cee9048cc72fd064ce5ae28f70a9216ea3b7155863f54393948c1b6d611a0be1532b6c9518b95ec56a0972a7'
